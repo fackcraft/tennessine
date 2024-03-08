b67a2ece0cd8203a7c92f734ea1d0bc280b97ec3
Ming Yue <mingfengpigeon@gmail.com>
2024-03-09 01:52:33 +0800

    Added radar map
    Added radar map. Added theme to config file. Fixed the reverse of left and right keys.

--- tennessine/main.py
+++ tennessine/main.py
@@ -11,6 +11,23 @@ logger: logging.Logger = logging.getLogger()
 config: configparser.ConfigParser = configparser.ConfigParser()
 config.read("config.ini")
 
+x: int = 0
+y: int = 0
+
+
+def draw_background(surface: pygame.surface.Surface, rect: Optional[
+        Tuple[
+            Union[float, int], Union[float, int], Union[float, int], Union[float, int]
+        ]
+    ] = None):
+    background: str = config.get("world", "background")
+    if background == "transgender_flag":
+        draw_transgender_flag(surface, rect)
+        return
+    if background == "rainbow_flag":
+        draw_rainbow_flag(surface, rect)
+        return
+    raise RuntimeError
 
 def draw_transgender_flag(
     surface: pygame.surface.Surface,
@@ -71,6 +88,44 @@ def draw_rainbow_flag(
     )
 
 
+class RadarMap(pygame.sprite.Sprite):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.image: pygame.surface.Surface = pygame.Surface(
+            (
+                config.getint("radarmap", "width"),
+                config.getint("radarmap", "width")
+                * config.getint("world", "height")
+                / config.getint("world", "width")
+            )
+        )
+        draw_background(self.image)
+
+        self.rect: pygame.rect.Rect = self.image.get_rect()
+        self.rect.x = config.getint("window", "width") - config.getint(
+            "radarmap", "width"
+        )
+
+    def update(self) -> None:
+        draw_background(self.image)
+        pygame.draw.rect(
+            self.image,
+            (255, 255, 255),
+            (
+                self.image.get_width() * x / config.getint("world", "width"),
+                self.image.get_height() * y / config.getint("world", "height"),
+                self.image.get_width()
+                * config.getint("window", "width")
+                / config.getint("world", "width"),
+                self.image.get_height()
+                * config.getint("window", "height")
+                / config.getint("world", "height"),
+            ),
+            1,
+        )
+
+
 if __name__ == "__main__":
     pygame.init()
     screen: pygame.surface.Surface = pygame.display.set_mode(
@@ -80,14 +135,15 @@ if __name__ == "__main__":
         (config.getint("world", "width"), config.getint("world", "height"))
     )
     clock: pygame.time.Clock = pygame.time.Clock()
-    draw_transgender_flag(
+    draw_background(
         world, (0, 0, config.getint("world", "width"), config.getint("world", "height"))
     )
-
+    group: pygame.sprite.Group = pygame.sprite.Group()
+    radar_map: RadarMap = RadarMap()
+    group.add(radar_map)
     running: bool = True
-    x: int = 0
-    y: int = 0
     delay: float = 0
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -100,14 +156,10 @@ if __name__ == "__main__":
             else:
                 y -= config.getint("move", "step")
         if keys[pygame.K_LEFT]:
-            if x + config.getint("move", "step") + config.getint(
-                "window", "width"
-            ) > config.getint("world", "width"):
-                x: int = config.getint("world", "width") - config.getint(
-                    "window", "width"
-                )
+            if x - config.getint("move", "step") < 0:
+                x: int = 0
             else:
-                x += config.getint("move", "step")
+                x -= config.getint("move", "step")
         if keys[pygame.K_DOWN]:
             if y + config.getint("move", "step") + config.getint(
                 "window", "height"
@@ -118,16 +170,23 @@ if __name__ == "__main__":
             else:
                 y += config.getint("move", "step")
         if keys[pygame.K_RIGHT]:
-            if x - config.getint("move", "step") < 0:
-                x: int = 0
+
+            if x + config.getint("move", "step") + config.getint(
+                "window", "width"
+            ) > config.getint("world", "width"):
+                x: int = config.getint("world", "width") - config.getint(
+                    "window", "width"
+                )
             else:
-                x -= config.getint("move", "step")
+                x += config.getint("move", "step")
 
         background: pygame.surface.Surface = world.subsurface(
             (x, y, config.getint("window", "width"), config.getint("window", "height"))
         )
         screen.blit(background, (0, 0))
 
+        group.update()
+        group.draw(screen)
         pygame.display.flip()
 
         delay: float = clock.tick(60) / 1000
